1c78a406c85a4349c49d00e19dcf6bf3bdc58e25
shaurya002 <shauryapratapsingh112@gmail.com>
2026-07-25 23:34:33 +0530

    BookService - Book Creation Implemented

--- src/main/java/com/shaurya/librarymanagementsystem/service/impl/BookServiceImpl.java
+++ src/main/java/com/shaurya/librarymanagementsystem/service/impl/BookServiceImpl.java
@@ -2,19 +2,46 @@ package com.shaurya.librarymanagementsystem.service.impl;
 
 import com.shaurya.librarymanagementsystem.dto.request.BookRequest;
 import com.shaurya.librarymanagementsystem.dto.response.BookResponse;
+import com.shaurya.librarymanagementsystem.mapper.BookMapper;
+import com.shaurya.librarymanagementsystem.model.entity.Author;
+import com.shaurya.librarymanagementsystem.model.entity.Book;
+import com.shaurya.librarymanagementsystem.model.enums.BookStatus;
+import com.shaurya.librarymanagementsystem.repositories.BookRepository;
 import com.shaurya.librarymanagementsystem.service.BookService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
     @Override
+    @Transactional
     public BookResponse createBook(BookRequest bookRequest){
-        return null;
+        if(bookRepository.existsByIsbn(bookRequest.isbn())){
+            throw new DuplicateIsbnException("Book with ISBN " + bookRequest.isbn() + " already exists.");
+        }
+        List<Author> authors = authorRepository.findAllById(bookRequest.authorIds());
+        if(authors.size()!=bookRequest.authorIds().size()){
+            throw new AuthorNotFoundException("Author Ids do not match.");
+        }
+        Book book = bookMapper.toEntity(
+                bookRequest,
+                new HashSet<>(authors)
+        );
+
+        book.setAvailableCopies(book.getTotalCopies());
+        book.setStatus(BookStatus.AVAILABLE);
+
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toResponse(savedBook);
     }
 
     @Override
