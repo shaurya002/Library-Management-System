cb32d0d7d5caa14b19a869bf283c64c9fd1f5a50
shaurya002 <shauryapratapsingh112@gmail.com>
2026-07-31 09:24:00 +0530

    Exception Handling - AuthorDeletionException & DuplicateAuthorException Implemented

--- src/main/java/com/shaurya/librarymanagementsystem/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/shaurya/librarymanagementsystem/service/impl/AuthorServiceImpl.java
@@ -2,6 +2,7 @@ package com.shaurya.librarymanagementsystem.service.impl;
 
 import com.shaurya.librarymanagementsystem.dto.request.AuthorRequest;
 import com.shaurya.librarymanagementsystem.dto.response.AuthorResponse;
+import com.shaurya.librarymanagementsystem.exception.AuthorDeletionException;
 import com.shaurya.librarymanagementsystem.exception.AuthorNotFoundException;
 import com.shaurya.librarymanagementsystem.exception.DuplicateAuthorException;
 import com.shaurya.librarymanagementsystem.mapper.AuthorMapper;
