5d8601b95389f1835cb99e51b8f3b18ca6d611a0
shaurya002 <shauryapratapsingh112@gmail.com>
2026-08-15 08:07:07 +0530

    feat: Added Spring Security configuration and user roles

---
+++ src/main/java/com/shaurya/librarymanagementsystem/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.shaurya.librarymanagementsystem.model.entity;
+
+import com.shaurya.librarymanagementsystem.model.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
